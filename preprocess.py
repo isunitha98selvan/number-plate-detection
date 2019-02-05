e6c44dc57cd1ef61d210969a3f7dff9888ec1456
anumehaagrawal <anuzenith29@gmail.com>
2019-02-05 20:24:01 +0530

    Localisation

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 
 def canny_edge(image):
-	edges=cv2.Canny(image,100,200)
+	edges=cv2.Canny(image,170,200)
 	cv2.imwrite('HPIM0942(2).png',edges)
+	
 
 def prewitt_edge(image):
 	img_gaussian = cv2.GaussianBlur(image,(3,3),0)
@@ -24,19 +25,41 @@ def sobel_edge(image):
 
 def main():
 	#loads image in grayscale
-	img=cv2.imread('HPIM0942.JPG',0)
+	img=cv2.imread('car.jpg')
+	img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	
 	cv2.imwrite('HPIM0942(1).png',img)
+	img = cv2.bilateralFilter(img, 11, 17, 17)
 	#various edge detection algorithms
 	canny_edge(img)
-	prewitt_edge(img)
-	sobel_edge(img)
-	#Binarising the image
-	cv2.imwrite('HPIM0942(2).png',img)
-	#OTSU Binarization
-	ret3,binary_img = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-	#th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-	cv2.imwrite('binarised_img.png',binary_img)
 	
+	#Binarising the image
+	img_bin=cv2.imread('HPIM0942(2).png',0)
+	#OTSU Binarization
+	ret3,binary_img = cv2.threshold(img_bin,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+	cv2.imwrite('binarised_img.png',binary_img)
+	#Finding contours
+	(cnts, _) = cv2.findContours(binary_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	cnts=sorted(cnts, key = cv2.contourArea, reverse = True)[0:2] 
+	detected_plate_contour = None 
+	
+
+	# loop over our contours to find the best possible approximate contour of number plate
+	count = 0
+	for c in cnts:
+		perimeter = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.02 * perimeter, True)
+		if len(approx) == 4:  # Select the contour with 4 corners
+			detected_plate_contour = approx 
+			break
+
+	print(detected_plate_contour)
+
+	# Drawing the selected contour on the original image
+	cv2.drawContours(img,[detected_plate_contour], -1, (0,255,0), 3)
+	cv2.imshow("Number plate", img)
+
+	cv2.waitKey(0)
 
 if __name__=='__main__':
 	main()
